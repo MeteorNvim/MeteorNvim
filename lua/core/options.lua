acebf96233cc94f3780e733ea8220e7264325f96
Fadel <59105868+SrWither@users.noreply.github.com>
2023-12-11 15:46:04 -0300

    Update options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -75,3 +75,6 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 vim.opt.foldlevelstart = 99
+
+-- Remove fill chars
+vim.opt.fillchars = {eob = " "}
