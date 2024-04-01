b79df38cc1bf696bccc2d81207e3721fb8a37adc
Fadel <59105868+SrWither@users.noreply.github.com>
2024-04-01 14:53:05 -0300

    null-ls -> none-ls

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -112,7 +112,7 @@ return {
       event = { "BufReadPre", "BufNewFile" },
       dependencies = {
         "williamboman/mason.nvim",
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
       },
       config = function()
         require("plugins.config.null_ls")
