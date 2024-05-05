e6b63b2468328ec85718b6e33bc8468410214877
Fadel <fjachuf@gmail.com>
2024-05-04 23:02:05 -0300

    bufferline error fixed

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,6 +4,7 @@ return {
         dependencies = {
             "nvim-tree/nvim-web-devicons"
         },
+        tag = "v4.5.3",
         config = function()
             require("plugins.config.bufferline")
         end
